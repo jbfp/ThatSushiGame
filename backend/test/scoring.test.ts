626041a939f40517211b0b82be026a5af2583a66
jbfp <jakob@jbfp.dk>
2020-04-24 19:21:06 +0200

    Add tests for scoreWasabiFaceUpCard

--- backend/test/scoring.test.ts
+++ backend/test/scoring.test.ts
@@ -1,5 +1,6 @@
 import * as sut from "../src/models/scoring";
-import { Nigiri, CardKind, NigiriKind } from "../src/models/card";
+import { Nigiri, CardKind, NigiriKind, Wasabi } from "../src/models/card";
+import { WasabiFaceUpCard, FaceUpCardKind } from "../src/models/faceUpCards";
 
 describe("Scoring", () => {
     describe("scoreNigiri", () => {
@@ -19,6 +20,32 @@ describe("Scoring", () => {
             const card: Nigiri = { kind: CardKind.Nigiri, nigiriKind: NigiriKind.Squid };
             const actual = sut.scoreNigiri(card);
             expect(actual).toBe(3);
+        });
+    });
+
+    describe("scoreWasabiFaceUpCard", () => {
+        const scoreWasabiFaceUpCard = (nigiri: Nigiri): number => {
+            const wasabi: Wasabi = { kind: CardKind.Wasabi };
+            const faceUpCard: WasabiFaceUpCard = { kind: FaceUpCardKind.Wasabi, nigiri, wasabi };
+            return sut.scoreWasabiFaceUpCard(faceUpCard);
+        };
+
+        test("Egg", () => {
+            const nigiri: Nigiri = { kind: CardKind.Nigiri, nigiriKind: NigiriKind.Egg };
+            const actual = scoreWasabiFaceUpCard(nigiri);
+            expect(actual).toBe(3);
+        });
+
+        test("Salmon", () => {
+            const nigiri: Nigiri = { kind: CardKind.Nigiri, nigiriKind: NigiriKind.Salmon };
+            const actual = scoreWasabiFaceUpCard(nigiri);
+            expect(actual).toBe(6);
+        });
+
+        test("Squid", () => {
+            const nigiri: Nigiri = { kind: CardKind.Nigiri, nigiriKind: NigiriKind.Squid };
+            const actual = scoreWasabiFaceUpCard(nigiri);
+            expect(actual).toBe(9);
         });
     });
 });
